79d241b37c2e951ac62cee296eb5951ccee5e7be
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2023-04-11 17:08:46 +0100

    fixed the hidden nav menu

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ const mobileNav = document.querySelector(".nav-section > nav > ul");
 burgerMenu.addEventListener("click", () => {
   mobileNav.classList.toggle("open");
 });
+
+document.querySelector("body").addEventListener("click", (e) => {
+  if (
+    mobileNav.classList.contains("open") &&
+    e.target !== mobileNav &&
+    e.target !== burgerMenu
+  ) {
+    mobileNav.classList.remove("open");
+  }
+});
